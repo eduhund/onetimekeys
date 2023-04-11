63d47135608d99568b6ad8a3363526b6c15169e8
Roman Nebel <im.roman.nebel@gmail.com>
2023-04-11 15:03:22 +0200

    Update OTK server

--- index.js
+++ index.js
@@ -8,28 +8,40 @@ const port = process.env.SERVER_PORT;
 app.use(express.json());
 app.use(require("body-parser").urlencoded({ extended: false }));
 
-const { oneTimeKeys } = require("./otkMachine");
+const OTK = require("./otkMachine");
 
+//Admin API. Генерация одноразового кода для сброса пароля.
 app.post("/set_otk", (req, res) => {
-  const user = req.body;
-  const secureKey = oneTimeKeys.setKey(user);
-  if (secureKey) {
-    res.status(200);
-    res.send({ OK: true, data: secureKey });
-  } else {
-    res.status(400);
-    res.send({ OK: false, error: "otk not generated" });
-  }
+	const user = req.body;
+	console.log("New OTK set: ", user);
+	const key = OTK.setKey(user);
+	if (key) {
+		res.status(200).send({ OK: true, data: key });
+	} else {
+		res.status(400).send({ OK: false, error: "OTK not generated" });
+	}
 });
 
-//Admin API. Генерация ссылки для сброса пароля.
+//Admin API. Проверка валидности одноразового кода.
 app.get("/check_otk", (req, res) => {
-  const key = req.query.key;
-  const result = oneTimeKeys.checkKey(key);
-  res.status(200);
-  res.send(result);
+	const { key } = req.query;
+	console.log("New OTK use: ", key);
+	const result = OTK.checkKey(key);
+	res.status(200).send(result);
+});
+
+//Admin API. Просмотр списка активных одноразовых ключей.
+app.get("/otk_list", (req, res) => {
+	const { verify } = req.query;
+	console.log("New OTK list check");
+	if (verify !== process.env.ADMIN_VERIFY) {
+		res.sendStatus(401);
+	} else {
+		const result = OTK.checkList();
+		res.status(200).send(result);
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+	console.log(`OTK server starts on port ${port}`);
 });
--- otkMachine.js
+++ otkMachine.js
@@ -1,47 +1,63 @@
 const fs = require("fs");
 
-function oneTimeKeyMachine() {
-  const keys = require("./oneTimeKeys.json");
-  function checkKey(key) {
-    if (Object.keys(keys).includes(key)) {
-      delete keys[key];
-      fs.writeFileSync(
-        require.resolve("./oneTimeKeys.json"),
-        JSON.stringify(keys)
-      );
-      return true;
-    } else {
-      return false;
-    }
-  }
+let keys = require("./oneTimeKeys.json");
 
-  function setKey(user) {
-    if (!user.id) {
-      return undefined;
-    }
-    const newKey = Math.random().toString(36).substring(2);
-    keys[newKey] = {
-      id: user.id,
-      expiresAt: Date.now() + 120 * 60 * 60 * 1000,
-    };
-    fs.writeFileSync(
-      require.resolve("./oneTimeKeys.json"),
-      JSON.stringify(keys)
-    );
-    return newKey;
-  }
-
-  function checkList() {
-    return keys;
-  }
-  return {
-    checkKey,
-    setKey,
-    checkList,
-  };
+function write(keys) {
+	fs.writeFileSync(require.resolve("./oneTimeKeys.json"), JSON.stringify(keys));
 }
 
-const oneTimeKeys = oneTimeKeyMachine();
+function oneTimeKeyMachine() {
+	function checkKey(key) {
+		try {
+			const keyIndex = keys.findIndex((item) => item.key === key);
+			if (!(keyIndex + 1)) {
+				return false;
+			}
 
-module.exports.oneTimeKeyMachine = oneTimeKeyMachine;
-module.exports.oneTimeKeys = oneTimeKeys;
+			const now = Date.now();
+			const { expiresAt } = keys[keyIndex];
+
+			keys.splice(keyIndex, 1);
+			write(keys);
+			if (expiresAt < now) {
+				return false;
+			} else return true;
+		} catch {
+			throw new Error("Check OTK failed");
+		}
+	}
+
+	function setKey(user) {
+		if (!user.id) {
+			throw new Error("User ID is undefined");
+		}
+
+		try {
+			const newKey = Math.random().toString(36).substring(2);
+			keys = keys.filter((item) => item.userId !== user.id);
+			keys.push({
+				key: newKey,
+				userId: user.id,
+				expiresAt: Date.now() + 120 * 60 * 60 * 1000,
+			});
+			write(keys);
+			return newKey;
+		} catch (e) {
+			console.log(e);
+			throw new Error("Set OTK failed");
+		}
+	}
+
+	function checkList() {
+		return keys;
+	}
+	return {
+		checkKey,
+		setKey,
+		checkList,
+	};
+}
+
+const OTK = oneTimeKeyMachine();
+
+module.exports = OTK;
