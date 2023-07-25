cd47edefa872f69e86d16d14a325032f7a751010
Roman Nebel <im.roman.nebel@gmail.com>
2023-07-25 14:59:40 +0200

    Fix responce

--- src/services/express/express.js
+++ src/services/express/express.js
@@ -30,7 +30,7 @@ app.get("/checkKey", (req, res) => {
 		res.status(200).send({ OK: true });
 	} else {
 		log("Error with key: ", key);
-		res.status(400).send({ OK: false, error: "OTK not generated" });
+		res.status(400).send({ OK: false, error: "OTK is invalid" });
 	}
 });
 
