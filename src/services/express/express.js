0ae4ba506d8fa1e0aa96922a78bd0f6ee1324706
Roman Nebel <im.roman.nebel@gmail.com>
2023-07-26 15:47:41 +0200

    Add more logs

--- src/services/express/express.js
+++ src/services/express/express.js
@@ -12,12 +12,13 @@ const OTK = require("../../core/otkMachine");
 //Admin API. Генерация одноразового кода для сброса пароля.
 app.post("/setKey", (req, res) => {
 	const user = req.body;
-	log("New OTK set: ", user);
 	const key = OTK.setKey(user);
 	if (key) {
+		log(`New OTK (${key}) set to user ${user.id}`);
 		res.status(200).send({ OK: true, data: { key } });
 	} else {
-		res.status(400).send({ OK: false, error: "OTK not generated" });
+		log(`OTK not generated to user ${user.id}`);
+		res.status(400).send({ OK: false, error: `OTK not generated to user` });
 	}
 });
 
