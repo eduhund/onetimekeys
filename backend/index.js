39bb69c6d14ec10f8ac263f01c3a6a38a9e88cb7
Roman Nebel <im.roman.nebel@gmail.com>
2023-08-27 16:57:31 +0200

    Add mongo Rename express files

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const { log } = console;
 
-const server = require("./src/services/express/express");
+const server = require("./src/services/server/server");
+const db = require("./src/services/db/db");
 
 async function run() {
 	await server.start();
+	await db.connect();
 	log("All systems is running. Let's rock!");
 }
 
