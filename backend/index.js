b0e099a3db7c43b1699b8574f27be75bc76d5498
Roman Nebel <im.roman.nebel@gmail.com>
2024-09-04 21:24:18 +0200

    Fix the longtime queue rules

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ const { log } = require("./src/services/logs/logs");
 
 const server = require("./src/services/server/server");
 const db = require("./src/services/db/db");
+const cron = require("./src/services/cron/cron");
 
 async function run() {
-	await server.start();
-	await db.connect();
-	log.info("All systems is running. Let's rock!");
+  await server.start();
+  await db.connect();
+  await cron.schedule();
+
+  log.info("All systems is running. Let's rock!");
 }
 
 run();
--- backend/src/core/otkMachine.js
+++ backend/src/core/otkMachine.js
@@ -1,107 +1,139 @@
 const { log } = require("../services/logs/logs");
 const { generateKey } = require("keycrafter");
-const { readFile, writeFile } = require("../services/fs/fs")
+const { readFile, writeFile } = require("../services/fs/fs");
 
 function oneTimeKeyMachine() {
-	const burnQueue = {}
-	const keys = readFromDump()
-	
+  const burnQueue = {};
+  const keys = readFromDump();
 
-	function readFromDump() {
-		const now = Date.now()
-		const initKeys = readFile("/files/", "otk.json",) || [];
-		log.debug("Init previous keys:", initKeys.length)
-		const filtered = initKeys.filter((key) => key.expiresAt > now)
-		for (const key of filtered) {
-			const timeout = key.expiresAt - now 
-			addToBurnQueue(key, timeout > 0 ? timeout : 0)
-		}
+  function readFromDump() {
+    const now = Date.now();
+    const initKeys = readFile("/files/", "otk.json") || [];
+    log.debug("Init previous keys:", initKeys.length);
+    const filtered = initKeys.filter((key) => key.expiresAt > now);
+    for (const key of filtered) {
+      const timeout = key.expiresAt - now;
+      addToBurnQueue(key, timeout);
+    }
 
-		writeFile("/files/", "otk.json", filtered)
+    writeFile("/files/", "otk.json", filtered);
 
-		return filtered
-	}
+    return filtered || [];
+  }
 
-	function writeToDump() {
-		writeFile("/files/", "otk.json", keys)
-	}
+  function writeToDump() {
+    writeFile("/files/", "otk.json", keys);
+  }
 
-	function checkKey(key) {
-		try {
-			const keyIndex = keys.findIndex((item) => item.key === key);
-			if (!(keyIndex + 1)) {
-				return false;
-			}
+  function checkKey(key) {
+    try {
+      const keyIndex = keys.findIndex((item) => item.key === key);
+      if (!(keyIndex + 1)) {
+        return false;
+      }
 
-			const { userId, orgId, created, isReusable } = keys[keyIndex];
+      const { userId, orgId, created, isReusable } = keys[keyIndex];
 
-			if (!isReusable) {
-				keys.splice(keyIndex, 1)
-				clearTimeout(burnQueue[userId+orgId+created])
-				writeToDump()
-				log.debug(`OTK (${key}) was removed from the org ${orgId}`)
-			}
+      if (!isReusable) {
+        keys.splice(keyIndex, 1);
+        clearTimeout(burnQueue[userId + orgId + created]);
+        writeToDump();
+        log.debug(`OTK (${key}) was removed from the org ${orgId}`);
+      }
 
-			return true;
-		} catch {
-			throw new Error("Check OTK failed");
-		}
-	}
+      return true;
+    } catch {
+      throw new Error("Check OTK failed");
+    }
+  }
 
-	function setKey(data, orgId) {
-		const { userId, type = "digit", length = 4, life = 3600, isReusable = false } = data;
-		if (!userId || !orgId) {
-			throw new Error("No UserID or OrgID");
-		}
+  function setKey(data, orgId) {
+    const {
+      userId,
+      type = "digit",
+      length = 4,
+      life = 3600,
+      isReusable = false,
+    } = data;
+    if (!userId || !orgId) {
+      throw new Error("No UserID or OrgID");
+    }
 
-		const timeout = life * 1000
+    try {
+      const timeout = life * 1000;
+      const daysOfLife = life / (3600 * 24);
+      const newKey = generateKey({ type, length });
+      //keys = keys.filter((item) => item.userId !== user.id);
+      const now = Date.now();
+      const key = {
+        key: newKey,
+        userId,
+        orgId,
+        isReusable,
+        created: now,
+        expiresAt: now + timeout,
+      };
+      keys.push(key);
+      writeToDump();
 
-		try {
-			const newKey = generateKey({ type, length });
-			//keys = keys.filter((item) => item.userId !== user.id);
-			const now = Date.now()
-			const key = {
-				key: newKey,
-				userId,
-				orgId,
-				isReusable,
-				created: now,
-				expiresAt: now + timeout,
-			}
-			keys.push(key);
-			addToBurnQueue(key, timeout)
-			writeToDump()
+      if (daysOfLife > 21) {
+        addToBurnQueue(key, timeout);
+      }
 
-			return newKey;
-		} catch (e) {
-			log.error(e);
-			throw new Error("Set OTK failed");
-		}
-	}
+      return newKey;
+    } catch (e) {
+      log.error(e);
+      throw new Error("Set OTK failed");
+    }
+  }
 
-	function addToBurnQueue({key, userId, orgId, created}, timeout) {
-		log.debug(`OTK (${key}) for org ${orgId} was added to the burn queue`)
-		const queueItem = setTimeout(() => {
-			const index = keys.findIndex((item) => item.orgId === orgId && item.key === key )
-			if (index !== -1) {
-				keys.splice(index, 1)
-				writeToDump()
-				log.debug(`OTK (${key}) was removed from the org ${orgId}`)
-			}
-		}, timeout)
+  function addToBurnQueue({ key, userId, orgId, created }, timeout) {
+    const daysOfLife = timeout / (1000 * 3600 * 24);
 
-		burnQueue[userId+orgId+created] = queueItem
-	}
+    if (timeout <= 0 && daysOfLife > 21) {
+      return;
+    }
 
-	function checkList() {
-		return keys;
-	}
+    const queueItem = setTimeout(() => {
+      const index = keys.findIndex(
+        (item) => item.orgId === orgId && item.key === key
+      );
+      if (index !== -1) {
+        keys.splice(index, 1);
+        writeToDump();
+        log.debug(`OTK (${key}) was removed from the org ${orgId}`);
+      }
+    }, timeout);
 
-	return {
-		checkKey,
-		setKey,
-		checkList,
-	};
+    burnQueue[userId + orgId + created] = queueItem;
+
+    log.debug(`OTK (${key}) for org ${orgId} was added to the burn queue`);
+  }
+
+  function checkList() {
+    return keys;
+  }
+
+  function updateDump() {
+    const oldKeysQt = keys.length;
+    const newKeys = readFromDump();
+    const newKeysQt = newKeys.length;
+
+    const delta = oldKeysQt - newKeysQt;
+
+    if (delta > 1) {
+      log.debug(`Automatically deleted ${delta} keys`);
+    } else if (delta === 1) {
+      log.debug(`Automatically deleted 1 key`);
+    }
+  }
+
+  return {
+    checkKey,
+    setKey,
+    checkList,
+    updateDump,
+  };
 }
 
 const OTK = oneTimeKeyMachine();
